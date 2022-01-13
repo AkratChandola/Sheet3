5d39c070b1b601565c7e5338b4bd2ebb718e2603
Akrat Chandola <akratchandola@gmail.com>
2022-01-13 19:16:32 +0530

    Add files via upload

---
+++ 3_1.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double sum=0;
+	int i,n;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+1.0/(i*i);
+	}
+	cout<<sum<<endl;
+	return 0;
+}
