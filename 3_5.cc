92ebf92336e245484223f663e05f6190edc954a3
Akrat Chandola <akratchandola@gmail.com>
2022-01-14 13:21:25 +0530

    Update 3_5.cc

--- 3_5.cc
+++ 3_5.cc
@@ -3,28 +3,18 @@ using namespace std;
 int main()
 {
 	// 1 2 3 6 9 18 27 54 .....
-	int i,n,k=3,m=0,sum=0,total=0;
+	int i,n,k=0,m=1,sum=1,total=1;
 	cin>>n;
 	for(i=1;i<=n;i++)
 	{
-		if(i<=3){
-			sum++;
-			total+=sum;
-			//cout<<sum<<" ";			
-		}
-
-		else
+		if(m==3)
 		{
-			m++;
-			sum+=k;
-			total+=sum;
-			//cout<<sum<<" ";
-			if(m==2)
-			{
-				m=0;
-				k=k*3;
-			}
+			m=1;
+			k++;
 		}
+		sum=sum+pow(3,k);
+		m++;
+		total=total+sum;
 	}
 	cout<<total<<endl;
 	return 0;
